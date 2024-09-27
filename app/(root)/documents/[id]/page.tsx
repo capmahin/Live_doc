30d9b1ab6b31728c78838e2c1c4007a25168f0bf
capmahin <mahinulhuque1996@gmail.com>
2024-09-27 20:36:50 +0600

    add sign in and sign up

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const page = () => {
-  return <div>page</div>;
+const Document = () => {
+  return <div>Document</div>;
 };
 
-export default page;
+export default Document;
