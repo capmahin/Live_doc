22df60be19427d48e4ee1111015c313c1c2daf56
capmahin <mahinulhuque1996@gmail.com>
2024-10-03 19:17:03 +0600

    add with and heighyt

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,13 @@ const Header = () => {
   return (
     <div className="header">
       <Link href="/" className="md:flex-1">
-        <Image src="/assets/icons/logo.svg" alt="Logo with name" />
+        <Image
+          src="/assets/icons/logo.svg"
+          alt="Logo with name"
+          width={120}
+          height={32}
+          className="hidden md:block"
+        />
       </Link>
     </div>
   );
