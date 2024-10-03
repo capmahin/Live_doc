d6821e2c8a7a7f8c06553cfdb8c938551af830d0
capmahin <mahinulhuque1996@gmail.com>
2024-10-03 19:02:09 +0600

    add a link

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <Link href="/" className="md:flex-1"></Link>
+    </div>
+  );
 };
 
 export default Header;
