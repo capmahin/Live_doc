86e85212fde374e825a57a0f91a7adcc3ba92b75
capmahin <mahinulhuque1996@gmail.com>
2024-10-03 19:07:21 +0600

    add a image

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,13 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 const Header = () => {
   return (
     <div className="header">
-      <Link href="/" className="md:flex-1"></Link>
+      <Link href="/" className="md:flex-1">
+        <Image src="/assets/icons/logo.svg" alt="Logo with name" />
+      </Link>
     </div>
   );
 };
