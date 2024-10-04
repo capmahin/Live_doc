7f72d10e38c6b74f0b5fc1487f3950d35d7f717c
capmahin <mahinulhuque1996@gmail.com>
2024-10-04 11:45:40 +0600

    add a p tag

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -5,7 +5,9 @@ const Document = () => {
   return (
     <div>
       <Header>
-        <div className="flex w-fit items-center justify-center gap-2"></div>
+        <div className="flex w-fit items-center justify-center gap-2">
+          <p className="document-title">This is a fake document title</p>
+        </div>
       </Header>
       <Editor />
     </div>
